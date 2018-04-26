16df08d48c6a4b44425d985315e3fb0de6e72c92
maximiliano <smartmaxdev@gmail.com>
2018-04-27 04:36:52 +0800

    sss

--- views/about/about.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('myApp.about', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/about', {
-    templateUrl: 'views/about/about.html',
-  });
-}]);
